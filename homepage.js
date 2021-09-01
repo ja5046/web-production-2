4ce483e14dbd8407c6109e55d1b64214368cf88d
justin Alpizar <justin.alpizar96@myhunter.cuny.edu>
2021-09-01 15:59:02 -0400

    homepage

---
+++ homepage.js
@@ -0,0 +1,35 @@
+console.log('hello');
+
+let x = 5;
+
+x=5;
+console.log(x)
+
+
+let me = {name: 'Justin', adjectives: ['working out', 'rollerblading', 'playing video games', 'watching movies']}
+
+function describeMe() {
+  document.querySelector("description").innerHTML = me.name + " enjoys " + getRandomItem(me.adjectives)
+}
+
+
+function changeColor() {
+  document.querySelector('#color-change').style.backgroundColor = 'red'
+
+}
+
+
+let y = 0
+
+function doMath() {
+  //this automatically increases y by 1
+  y++
+  document.querySelector('#math').innerHTML = y
+}
+
+function getRandomItem(array) {
+  //don't worry about the math here, this just selects a random item from the array
+  //but it could be useful if you want to do something similar!
+
+return array[Math.floor(Math.random()array.length)]
+}
